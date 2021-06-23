e68343fdee0e01b40685dcebddef4c6e0bfd086e
Sofia Tejada <sofianais@icloud.com>
2021-06-23 15:48:39 -0700

    write functins in utls.js

--- src/Item.js
+++ src/Item.js
@@ -5,7 +5,10 @@ export default class Item extends Component {
     render() {
         return (
             <div>
-                <Link></Link>
+                <Link>
+                    <img src={this.props.poop.image_url} alt={this.props.poop.name} />
+                    <h4>{this.props.poop.name}</h4>
+                </Link>
             </div>
         )
     }
